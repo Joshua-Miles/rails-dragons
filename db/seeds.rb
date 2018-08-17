b9f5df5c3f2d9a20d70d1f77c022efb0a8413682
ELehotzky <Lehotzky.Erica@gmail.com>
2018-08-17 09:26:42 -0500

    60 minute commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Dragon.destroy_all
+
+
+dany = User.create(name: "Daenerys Stormborn", house: "Targaryen")
+john = User.create(name: "John Snow", house: "Stark")
+tyrion = User.create(name: "Tyrion", house: "Lannister")
+arya = User.create(name: "Arya", house: "Stark")
+night = User.create(name: "The Night King", house: "Dead")
+
+drogo = Dragon.create(name: "Drogon", color: "black and red", user_id: 1)
+viserion = Dragon.create(name: "Viserion", color: "green and bronze", user_id: 5)
+rhaegal = Dragon.create(name: "Rhaegal", color: "cream and gold", user_id: 2)
