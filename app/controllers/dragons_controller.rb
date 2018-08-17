2af43fb1e464640cf3af7cc007a68f5253d74e98
github username <github email address>
2018-08-17 10:45:41 -0500

    Time

---
+++ app/controllers/dragons_controller.rb
@@ -0,0 +1,41 @@
+class DragonsController < ApplicationController
+  def index
+    @dragons = Dragon.all
+  end
+
+  def new
+    @dragon = Dragon.new
+  end
+
+  def create
+    dragon = Dragon.create(dragon_params)
+    redirect_to dragon_path(dragon)
+  end
+
+  def show
+    @dragon = Dragon.find(params[:id])
+  end
+
+  def edit
+    @dragon = Dragon.find(params[:id])
+  end
+
+  def update
+    @dragon = Dragon.find(params[:id])
+    @dragon.update(dragon_params)
+    redirect_to dragon_path(@dragon)
+  end
+
+  def destroy
+    @dragon = Dragon.find(params[:id])
+    @dragon.destroy
+    redirect_to dragons_path
+  end
+
+  private
+
+  def dragon_params
+    params.require(:dragon).permit(:name, :age, :color, :user)
+  end
+  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+billy = User.create(name: 'Billy Bill', age: 20)
+suzy = User.create(name: 'Suzy Sue', age: 18)
+timmy = User.create(name: 'Timmy Tim', age: 14)
+
+blue  = Dragon.create(name: 'Blue', age: 5, color: 'blue', user: billy)
+green = Dragon.create(name: 'Green', age: 7, color: 'green', user: suzy)
+red = Dragon.create(name: 'Red', age: 8, color: 'red', user: timmy)
