418c0f60b85ac34d4a7b73bc1017d12e2205e839
Kelly <devkells00@gmail.com>
2018-08-19 19:20:38 -0500

    Partial completion

---
+++ db/migrate/20180817184419_create_dragons.rb
@@ -0,0 +1,10 @@
+class CreateDragons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dragons do |t|
+      t.string :name
+      t.integer :flames
+      t.belongs_to :user
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+kelly = User.create(first_name: "Kelly", last_name: "Lwakatare")
+emil = User.create(first_name: "Emil", last_name: "Mutalemwa")
+bibi = User.create(first_name: "Bibi", last_name: "Mzee")
+
+mateo = Dragon.create(name: "Mateo", flames: 8, user:kelly)
+isobel = Dragon.create(name: "Isobel", flames: 4, user:emil)
+jojo = Dragon.create(name: "Jojo", flames: 3, user:bibi)
